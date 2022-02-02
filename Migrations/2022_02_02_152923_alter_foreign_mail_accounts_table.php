5fe1ec28db01c68ce734ae2f784d5229a2fb5ffb
sash04ek <a.ovcharov@afterlogic.com>
2022-02-02 15:49:25 +0300

    fixed Issue with deleting a server

---
+++ Migrations/2022_02_02_152923_alter_foreign_mail_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class AlterForeignMailAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Capsule::schema()->table('mail_accounts', function (Blueprint $table) {
+            $table->dropForeign('mail_accounts_serverid_foreign');
+            $table->foreign('ServerId')->references('Id')->on('mail_servers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Capsule::schema()->table('mail_accounts', function (Blueprint $table) {
+            $table->dropForeign('mail_accounts_serverid_foreign');
+            $table->foreign('ServerId')->references('Id')->on('mail_servers');
+        });
+    }
+}
