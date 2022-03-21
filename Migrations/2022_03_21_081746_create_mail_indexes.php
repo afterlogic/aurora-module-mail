83b5c431d971d46a4584a31d59276c49be7deea7
sash04ek <a.ovcharov@afterlogic.com>
2022-03-21 08:30:05 +0300

    added indexes

---
+++ Migrations/2022_03_21_081746_create_mail_indexes.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class CreateMailIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Capsule::schema()->table('mail_accounts', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+        });
+
+        Capsule::schema()->table('mail_identities', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+            $table->index('IdAccount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Capsule::schema()->table('mail_accounts', function (Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+        });
+
+        Capsule::schema()->table('mail_identities', function (Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+            $table->dropIndex(['IdAccount']);
+        });
+    }
+}
