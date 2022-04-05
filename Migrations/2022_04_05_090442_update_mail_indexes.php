6e9e491c179ff635af2be82073ef619cc7463953
sash04ek <a.ovcharov@afterlogic.com>
2022-04-05 10:05:27 +0300

    updated db indexes

---
+++ Migrations/2022_04_05_090442_update_mail_indexes.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class UpdateMailIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Capsule::schema()->table('mail_domains', function(Blueprint $table)
+        {
+            $table->index('TenantId');
+            $table->index('MailserverId');
+        });
+        Capsule::schema()->table('mail_fetchers', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+            $table->index('IdAccount');
+        });
+        Capsule::schema()->table('mail_refresh_folders', function(Blueprint $table)
+        {
+            $table->index('IdAccount');
+        });
+        Capsule::schema()->table('mail_servers', function(Blueprint $table)
+        {
+            $table->index('TenantId');
+        });
+        Capsule::schema()->table('mail_system_folders', function(Blueprint $table)
+        {
+            $table->index('IdAccount');
+        });
+        Capsule::schema()->table('mail_trusted_senders', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Capsule::schema()->table('mail_domains', function (Blueprint $table)
+        {
+            $table->dropIndex(['IdTenant']);
+            $table->dropIndex(['MailserverId']);
+        });
+        Capsule::schema()->table('mail_fetchers', function(Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+            $table->dropIndex(['IdAccount']);
+        });
+        Capsule::schema()->table('mail_refresh_folders', function(Blueprint $table)
+        {
+            $table->dropIndex(['IdAccount']);
+        });
+        Capsule::schema()->table('mail_servers', function(Blueprint $table)
+        {
+            $table->dropIndex(['TenantId']);
+        });
+        Capsule::schema()->table('mail_system_folders', function(Blueprint $table)
+        {
+            $table->dropIndex(['IdAccount']);
+        });
+        Capsule::schema()->table('mail_trusted_senders', function(Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+        });
+    }
+}
